66f4b87fa5ce0c2b32a852e05672ce9a5c0534a8
dadadelly <delhataichanok@gmail.com>
2026-03-13 17:17:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || 'YOUR_CHANNEL_ACCESS_TOKEN',
-  channelSecret: process.env.CHANNEL_SECRET || 'YOUR_CHANNEL_SECRET',
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 const client = new line.Client(config);
 const app = express();
-app.use(express.json());
-
 
 // ===== FOOD DATABASE =====
 const foodMenu = [
